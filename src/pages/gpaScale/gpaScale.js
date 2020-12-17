bc94cb96530f9a3924c8b8f84823e11393f503ef
u.t.wadhwani-at-047117452485 <u.t.wadhwani@gmail.com>
2020-12-17 15:41:26 +0530

    Static page for showing GPA scale

--- src/pages/gpaScale/gpaScale.js
+++ src/pages/gpaScale/gpaScale.js
@@ -1,7 +1,87 @@
 import React, { Component } from "react";
+import { GPARange } from "../../util/constants";
 
 export default class GPAScale extends Component {
+  state = {
+    calculatedGPA: 0,
+  };
+  onLetterChange = (event) => {
+    GPARange.forEach((item) => {
+      if (item.Letter === event.target.value) {
+        this.setState({
+          selectedLetter: event.target.value,
+          selectedPercent: item.MinPercent,
+          calculatedGPA: item.GPAScale,
+        });
+      }
+    });
+  };
+  onPercentChange = (event) => {
+    GPARange.forEach((item) => {
+      if (item.MinPercent === parseInt(event.target.value)) {
+        this.setState({
+          selectedLetter: item.Letter,
+          selectedPercent: event.target.value,
+          calculatedGPA: item.GPAScale,
+        });
+      }
+    });
+  };
   render() {
-    return <div>GPAScale</div>;
+    return (
+      <div>
+        GPAScale
+        <table>
+          <tr>
+            <td>
+              <select
+                onChange={this.onLetterChange}
+                value={this.state.selectedLetter}
+              >
+                <option value="0">Select the letter</option>
+                {GPARange &&
+                  GPARange.map((data, index) => {
+                    return <option value={data.Letter}>{data.Letter}</option>;
+                  })}
+              </select>
+            </td>
+            <td>
+              <select
+                onChange={this.onPercentChange}
+                value={this.state.selectedPercent}
+              >
+                <option value="0">Select the GPA</option>
+                {GPARange &&
+                  GPARange.map((data, index) => {
+                    return (
+                      <option value={data.MinPercent}>
+                        {data.MinPercent}-{data.MaxPercent}
+                      </option>
+                    );
+                  })}
+              </select>
+            </td>
+            <td>
+              <strong> {this.state.calculatedGPA}</strong>
+            </td>
+          </tr>
+        </table>
+        <table>
+          <th>List of Common GPA Conversations</th>
+          {GPARange &&
+            GPARange.map((data, index) => {
+              return (
+                <tr key={index}>
+                  <td>{data.Letter}</td>
+                  <td>
+                    {data.MinPercent} - {data.MaxPercent}
+                  </td>
+                  <td>{data.GPAScale}</td>
+                </tr>
+              );
+            })}
+        </table>
+      </div>
+    );
   }
 }
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,5 +1,16 @@
-export default class constants {}
-
-export const defaultDeliveryCharges = 50;
 export const environment = "TEST";
 //export const environment = "LIVE";
+export const GPARange = [
+  { Letter: "A+", MinPercent: 97, MaxPercent: 100, GPAScale: 4.0 },
+  { Letter: "A", MinPercent: 93, MaxPercent: 96, GPAScale: 4.0 },
+  { Letter: "A-", MinPercent: 90, MaxPercent: 92, GPAScale: 3.7 },
+  { Letter: "B+", MinPercent: 87, MaxPercent: 89, GPAScale: 3.3 },
+  { Letter: "B", MinPercent: 83, MaxPercent: 86, GPAScale: 3.0 },
+  { Letter: "B-", MinPercent: 80, MaxPercent: 82, GPAScale: 2.7 },
+  { Letter: "C+", MinPercent: 77, MaxPercent: 79, GPAScale: 2.3 },
+  { Letter: "C", MinPercent: 73, MaxPercent: 76, GPAScale: 2.0 },
+  { Letter: "C-", MinPercent: 70, MaxPercent: 72, GPAScale: 1.7 },
+  { Letter: "D+", MinPercent: 67, MaxPercent: 69, GPAScale: 1.3 },
+  { Letter: "D", MinPercent: 65, MaxPercent: 66, GPAScale: 1.0 },
+  { Letter: "F", MinPercent: 0, MaxPercent: 64, GPAScale: 0.0 },
+];
