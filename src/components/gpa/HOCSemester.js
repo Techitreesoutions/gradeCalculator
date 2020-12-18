438b23df0a67b2d721b3951739e76f9b9e20f5a6
u.t.wadhwani-at-047117452485 <u.t.wadhwani@gmail.com>
2020-12-18 13:52:45 +0530

    add/delete semester

--- src/components/gpa/HOCSemester.js
+++ src/components/gpa/HOCSemester.js
@@ -1,12 +1,69 @@
 import React, { Component } from "react";
 import Semester from "./Semester";
+import { defaultSemesterCount } from "../../util/constants";
 export default class HOCSemester extends Component {
+  state = {
+    semesterCount: defaultSemesterCount,
+    semesterBlocks: [],
+  };
+
+  componentDidMount = () => {
+    this.init();
+  };
+
+  increaseSemesterBlock = () => {
+    var array = this.state.semesterBlocks;
+    var lastElement = array[array.length - 1];
+    debugger;
+    array.push(
+      <Semester
+        index={parseInt(lastElement.key) + 1}
+        decreaseSemesterBlock={this.decreaseSemesterBlock}
+        key={parseInt(lastElement.key) + 1}
+      />
+    );
+    this.setState({
+      semesterBlocks: array,
+    });
+  };
+
+  decreaseSemesterBlock = (key) => {
+    var array = this.state.semesterBlocks;
+    var index;
+    array.forEach((item, i) => {
+      if (parseInt(item.key) === key) {
+        index = i;
+      }
+    });
+    array.splice(index, 1);
+    this.setState({
+      semesterBlocks: array,
+    });
+  };
+
+  init = () => {
+    var requiredArray = [];
+    for (var i = 0; i < this.state.semesterCount; i++) {
+      requiredArray.push(
+        <Semester
+          index={i}
+          decreaseSemesterBlock={this.decreaseSemesterBlock}
+          key={i}
+        />
+      );
+    }
+
+    this.setState({
+      semesterBlocks: requiredArray,
+    });
+  };
+
   render() {
     return (
       <div className="box">
-        <Semester />
+        {this.state.semesterBlocks}
         <div>
-          <button>Add semester</button>
+          <button onClick={this.increaseSemesterBlock}>Add semester</button>
         </div>
         <span>calculated GPA value will be displayed here</span>
       </div>
--- src/components/gpa/Semester.js
+++ src/components/gpa/Semester.js
@@ -6,12 +6,12 @@ export default class Semester extends Component {
       <div>
         <div className="row">
           <div className="col-lg-3">
-            <h3>Semester</h3>
+            <h3>Semester {this.props.index}</h3>
           </div>
           <div className="col-lg-3"></div>
           <div className="col-lg-3">
             <div className="float-right">
-              <div class="checkbox">
+              <div className="checkbox">
                 <label>
                   <input type="checkbox" value="" />
                   Weighted
@@ -21,7 +21,12 @@ export default class Semester extends Component {
           </div>
           {/* x button to close or cut the semester Component */}
           <div className="col-lg-3">
-            <div className="float-right">x</div>
+            <div
+              className="float-right"
+              onClick={() => this.props.decreaseSemesterBlock(this.props.index)}
+            >
+              x
+            </div>
           </div>
         </div>
         <div className="row">
